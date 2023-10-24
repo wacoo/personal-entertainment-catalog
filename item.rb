1e733a34f57fc8c6e9a7f75ba3ea790a8b5d097b
Boniface Dennis <bonifacedennis40@yahoo.com>
2023-10-24 11:08:20 +0100

    display method was added

--- item.rb
+++ item.rb
@@ -1,25 +1,24 @@
 require 'date'
 
 class Item
-    attr_accessor :genre, :author, :source, :label, :published_date
-    attr_reader :id, :archived
+  attr_accessor :genre, :author, :source, :label, :published_date
+  attr_reader :id, :archived
 
-    def initialize(published_date)
-        @id = Random.rand(1..1000)
-        @published_date = published_date
-        @archived = false
-    end
+  def initialize(published_date)
+    @id = Random.rand(1..1000)
+    @published_date = published_date
+    @archived = false
+  end
 
-    def can_be_archived?
-        current_year = Time.now.year
-        item_year = Date.parse(@published_date).year
-        # Check if the item's published_date is older than 10 years
-        if (current_year - item_year > 10)
-          return true
-        else
-          return false
-        end
-      end
+  def can_be_archived?
+    current_year = Time.now.year
+    item_year = Date.parse(@published_date).year
+    # Check if the item's published_date is older than 10 years
+    return true if current_year - item_year > 10
+
+
+    false
+  end
 end
 
 # item = Item.new("2003-03-19")
