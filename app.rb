6a5fec1f9c0de9a4855723d154b336f1c3f33dd0
Mustakim <m.m.kiron@gmail.com>
2023-10-24 16:00:26 +0600

    Add exit and menu methods

--- app.rb
+++ app.rb
@@ -1,3 +1,11 @@
 class App
-    
+    def exit
+        puts ""
+        puts "**********************************"
+        puts "Thank you for using this app"
+        puts "Goodbye"
+        puts "**********************************"
+        puts ""
+        Kernel.exit
+    end    
 end
--- main.rb
+++ main.rb
@@ -1,7 +1,23 @@
 require_relative 'app'
 
 class Main
-    
+    def menu(app)
+        puts ""
+        puts "**********************************"
+        puts "* PERSONAL ENTERTAINMENT CATALOG *"
+        puts "**********************************"
+        puts ""
+        puts "SELECT OPTIONS (1 - 10)"
+        puts ""
+        # add options here
+        puts "  0) Exit"
+        puts ""
+        print "OPTION:> "
+        input = gets.chomp.to_i
+        puts ""
+        puts input
+        app.handle_logic(input)
+    end    
 end
 
 main = Main.new
