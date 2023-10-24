1fbdcc2a5441bcd1e47bd51db9117d92f61d5831
Boniface Dennis <bonifacedennis40@yahoo.com>
2023-10-24 14:41:15 +0100

    updated music album

--- music_album.rb
+++ music_album.rb
@@ -1,5 +1,14 @@
 class MusicAlbum
-  attr_accessor :on_spotify, 
-  def initialize()
+  attr_accessor :on_spotify
+
+  def initialize(on_spotify)
+    @on_spotify = on_spotify
+  end
+
+  def can_be_archived?
+    if super.can_be_archived? # Assuming there is a parent class with this method
+      return true if @on_spotify == true
+    end
+    false
   end
 end
