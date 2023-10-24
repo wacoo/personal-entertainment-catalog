bea643787f931bdc5b9ef5301cf552d5514703ef
wac <wabaham9@gmail.com>
2023-10-24 13:31:35 +0300

    Add Game and autor

---
+++ author.rb
@@ -0,0 +1,13 @@
+class author
+    def initialize (first_name, last_name)
+        @id = (101..200).to_a.shuffle
+        @first_name = first_name
+        @last_name = last_name
+        @items = []
+    end
+
+    def add_item(item)
+        item.author = self
+        @items << item unless @item.include(item)
+    end
+end
---
+++ game.rb
@@ -0,0 +1,31 @@
+require_relative('item')
+
+class Game < Item
+    def initialize(publish_date, multiplayer, last_played_at)
+        super(publish_date)
+        @multiplayer = multiplayer
+        @last_played_at = last_played_at
+    end
+    
+    def can_be_archived?()
+        parent_result = super
+        current_date = Date.today
+        parsed_date = Date.parse(@last_played_at)
+        years = current_date.year - parsed_date.year
+    
+        if current_date.month < parsed_date.month || (current_date.month == parsed_date.month && current_date.day < parsed_date.day)
+            years -= 1
+        end
+    
+        # puts years
+        if years > 2 && parent_result
+            true
+        else
+            false
+        end
+    end
+end
+
+# game = Game.new('2010/12/23', true, '2023/9/24')
+# puts game.can_be_archived?
+# puts game
