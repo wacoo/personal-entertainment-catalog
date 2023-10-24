0ebd05fab2a0c174a0e75f24a9accd98f2bd2886
wac <wabaham9@gmail.com>
2023-10-24 19:50:31 +0300

    Add ui

--- app.rb
+++ app.rb
@@ -1,9 +1,75 @@
+require_relative 'author'
+require_relative 'game'
 class App
+  def initialize
+    @authors = []
+    @games = []
+  end
+
+  def create_author
+    puts '**********************************'
+    puts '       CREATE A NEW AUTHOR        '
+    puts '**********************************'
+    puts ''
+    print 'First name:> '
+    fname = gets.chomp
+    print 'Last name:> '
+    lname = gets.chomp
+
+    author = Author.new(fname, lname)
+    @authors << author
+    return author
+  end
+
+  def create_game
+    puts '**********************************'
+    puts '        CREATE A NEW GAME        '
+    puts '**********************************'
+    puts ''
+    list_all_authors
+    puts ''
+    print 'Select author:> '
+
+    author = gets.chomp.to_i
+    print 'Publish date:> '
+    pub_date = gets.chomp
+    print 'Last played at[DATE]:> '
+    last_played_date = gets.chomp
+    print 'Multi-player[Y/N]:> '
+    multi = gets.chomp
+
+    multiplayer = false
+    game = nil
+    if multi.upcase == 'Y'
+      multiplayer = true
+    end
+
+    if author >= 1
+      game = Game.new(@authors[author - 1], pub_date, multiplayer, last_played_date)
+      @games << game
+    else
+      puts "Wrong input!"
+    end
+    return game
+  end
+
+  def list_all_authors
+    @authors.each_with_index do |author, idx|
+      puts "#{idx + 1}) #{author.first_name} #{author.last_name}"
+    end
+  end
+
+  def list_all_games
+    @games.each_with_index do |game, idx|
+      puts "#{idx + 1}) Game ID: #{game.id} Author: #{game.author.first_name} #{game.author.last_name} Last played: #{game.last_played_at}"
+    end
+  end
+
   def exit
     puts ''
     puts '**********************************'
-    puts 'Thank you for using this app'
-    puts 'Goodbye'
+    puts '*  Thank you for using this app. *'
+    puts '*           Goodbye 👋           *'
     puts '**********************************'
     puts ''
     Kernel.exit
@@ -11,7 +77,13 @@ class App
 
   def handle_logic(input)
     # add logic here
-    if input.zero?
+    if input == 3
+      list_all_games
+    elsif input == 4
+      create_author
+    elsif input == 5
+        create_game
+    elsif input.zero?
       self.exit
     else
       puts 'Wrong input!'
