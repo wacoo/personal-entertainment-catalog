c255987139d805045757b9e4ec61a19cd6fad7d7
wac <wabaham9@gmail.com>
2023-10-24 14:44:31 +0300

    Add getters and setters

--- game.rb
+++ game.rb
@@ -1,10 +1,12 @@
 require_relative('item')
 
 class Game < Item
-    def initialize(publish_date, multiplayer, last_played_at)
+    attr_accessor :multiplayer, :last_played_at
+    def initialize(author, publish_date, multiplayer, last_played_at)
         super(publish_date)
         @multiplayer = multiplayer
         @last_played_at = last_played_at
+        self.author = author
     end
     
     def can_be_archived?()
