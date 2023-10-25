f365daf24c04e4349ff499661a8d6bb74e5401ff
wac <wabaham9@gmail.com>
2023-10-25 11:23:32 +0300

    Fix linters

--- game.rb
+++ game.rb
@@ -1,34 +1,33 @@
 require_relative('item')
 
 class Game < Item
-    attr_accessor :multiplayer, :last_played_at
-    def initialize(genre, author, source, label, publish_date, multiplayer, last_played_at)
-        super(publish_date)
-        @multiplayer = multiplayer
-        @last_played_at = last_played_at
-        self.author = author
-        self.genre = genre
-        self.source = source
-        self.label = label
+  attr_accessor :multiplayer, :last_played_at
+
+  def initialize(params)
+    super(params[:publish_date])
+    @multiplayer = params[:multiplayer]
+    @last_played_at = params[:last_played_at]
+    self.author = params[:author]
+    self.genre = params[:genre]
+    self.source = params[:source]
+    self.label = params[:label]
+  end
+
+  def can_be_archived?()
+    parent_result = super
+    current_date = Date.today
+    parsed_date = Date.parse(@last_played_at)
+    years = current_date.year - parsed_date.year
+
+    years -= 1 if current_date.month < parsed_date.month || (current_date.month == parsed_date.month && current_date.day < parsed_date.day)
+
+    # puts years
+    if years > 2 && parent_result
+      true
+    else
+      false
     end
-    
-    def can_be_archived?()
-        parent_result = super
-        current_date = Date.today
-        parsed_date = Date.parse(@last_played_at)
-        years = current_date.year - parsed_date.year
-    
-        if current_date.month < parsed_date.month || (current_date.month == parsed_date.month && current_date.day < parsed_date.day)
-            years -= 1
-        end
-    
-        # puts years
-        if years > 2 && parent_result
-            true
-        else
-            false
-        end
-    end
+  end
 end
 
 # game = Game.new('2010/12/23', true, '2023/9/24')
