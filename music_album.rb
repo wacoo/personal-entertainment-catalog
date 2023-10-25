56adc1c181d343264106029e9621109d189621f8
Boniface Dennis <bonifacedennis40@yahoo.com>
2023-10-25 10:02:35 +0100

    updated changes

--- music_album.rb
+++ music_album.rb
@@ -1,8 +1,17 @@
-class MusicAlbum
+require_relative 'item'
+require_relative 'genre'
+
+class MusicAlbum < Item
   attr_accessor :on_spotify
 
-  def initialize(on_spotify)
+  def initialize(genre, author, source, label, published_date, on_spotify)
     @on_spotify = on_spotify
+    super.genre = genre
+    super.author = author
+    super.source = source
+    super.label = label
+    super.published_date = published_date
+    @archived = false
   end
 
   def can_be_archived?
