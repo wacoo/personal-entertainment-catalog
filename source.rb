d7a899dab50d7a2866e948e9177491feafd28932
Mustakim <m.m.kiron@gmail.com>
2023-10-27 00:42:27 +0600

    Create source class

---
+++ source.rb
@@ -0,0 +1,12 @@
+class source
+    attr_accessor :items
+
+    def initialize
+        @items = []
+    end
+
+    def add_item(item)
+        @items << item
+        item.source = self
+    end
+end
