8ebdd78e8b53848923153885aee7e2e2639baa6a
Bett Enock <bettenock968@gmail.com>
2023-10-26 11:46:29 -0700

    refactor book class.

--- book.rb
+++ book.rb
@@ -1,17 +1,14 @@
 require './item'
 
 class Book < Item
-  attr_accessor :name, :publisher, :published_date, :cover_state
+  attr_accessor :publisher, :published_date, :cover_state
 
-  def initialize(params)
-    super(params[:publish_date])
-    super.genre = params[:genre]
-    super.author = params[:author]
-    super.source = params[:source]
-    super.label = params[:label]
-    @publisher = params[:publisher]
-    @cover_state = params[:cover_state]
-  end
+def initialize(publisher, published_date, cover_state, )
+  super(published_date)
+  
+  @publisher = publisher
+  @cover_state = cover_state
+end
 
   def can_be_archived?
     super || @cover_state == 'bad'
