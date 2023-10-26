dcaca441e08f8bc84b825357dc66b21710ad6c94
Mustakim <m.m.kiron@gmail.com>
2023-10-27 01:04:38 +0600

    Add create movie and list movies methods for app.rb file

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require_relative 'game'
 require_relative 'book'
 require_relative 'label'
 require_relative 'genre'
+require_relative 'movie'
+require_relative 'source'
 
 class App
   def initialize
@@ -139,6 +141,52 @@ class App
     game
   end
 
+  def create_movie
+    puts ''
+    puts 'Creating a new movie...'
+
+    if @genres.empty?
+      puts 'Please add a genre first' 
+      # You may decide to create_genre here or give a notice to the user.
+    end
+
+    if @authors.empty?
+      puts 'Please add an author'
+      create_author
+    end
+
+    puts 'Choose author from the list by number:'
+    list_all_authors
+
+    author = gets.chomp.to_i
+
+    if author >= 1
+      publish_date = get_user_input('Enter movie publish-date [YYYY/MM/DD]: ')
+      silent = get_user_input('Is the movie silent? [true/false]: ')
+      silent = silent.downcase == 'true' ? true : false
+
+      movie = Movie.new(publish_date, silent: silent)
+      auth = @authors[author - 1]
+      movie.author = auth
+      # Optionally, you can assign a genre as with the Book.
+      @movies << movie
+    else
+      puts 'Wrong input'
+    end
+    puts 'Movie added successfully !!!'
+  end
+
+  def list_all_movies
+    puts 'Movie list is empty! Please add a movie' if @movies.empty?
+    puts ''
+    puts '________Movie List__________'
+    puts ''
+
+    @movies.each_with_index do |movie, idx|
+      puts "#{idx + 1}) Movie ID: #{movie.id}, Published Date: #{movie.published_date}, Silent: #{movie.silent}"
+    end
+  end
+
   def list_all_authors
     if @authors.empty?
       puts ''
@@ -179,6 +227,8 @@ class App
       2 => method(:list_all_games),
       3 => method(:list_all_books),
       4 => method(:list_all_labels),
+      5 => method(:list_all_movies),
+      6 => method(:create_movie),
       8 => method(:create_author),
       9 => method(:create_game),
       10 => method(:create_book),
