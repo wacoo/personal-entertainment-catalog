b0f191ed0ef53128e2a478815d2571c94a96d5d5
Bett Enock <bettenock968@gmail.com>
2023-10-27 12:13:19 -0700

    Create label operations.

---
+++ label_ops.rb
@@ -0,0 +1,33 @@
+class LabelOps
+  attr_reader :labels
+  def initialize
+    @labels = []
+  end
+
+  def get_user_input(message)
+    print message
+    gets.chomp
+  end
+
+  def create_label
+    puts ''
+    puts 'Adding New Label...'
+
+    title = get_user_input('Enter label title: ')
+    color = get_user_input('Enter label color: ')
+
+    label = Label.new(title, color)
+    @labels << label
+    
+    # puts 'Label created successfully !!!'
+  end
+
+  def list_all_labels
+    puts 'Labels are empty!!😭' if @labels.empty?
+    puts ''
+    puts '________LABELS__________'
+    @labels.each_with_index do |label, idx|
+      puts "#{idx + 1}) Color: #{label.title} Title:#{label.color}"
+    end
+  end
+end
