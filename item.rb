2661dcef831a354630863acaccc9e8d57d3ceed4
Boniface Dennis <bonifacedennis40@yahoo.com>
2023-10-28 16:42:06 +0100

    i ensured that the item class has one genre, author, label and source

--- item.rb
+++ item.rb
@@ -19,6 +19,26 @@ class Item
     false
   end
 
+  def add_genre(genre)
+    @genre = genre
+    genre.items << self unless genre.items.include?(self)
+  end
+
+  def add_author(author)
+    @author = author
+    author.items << self unless author.items.include?(self)
+  end
+
+  def add_label(label)
+    @label = label
+    label.items << self unless label.items.include?(self)
+  end
+
+  def add_source(source)
+    @source = source
+    source.items << self unless source.items.include?(self)
+  end
+
   # method for archive
   def move_to_archive
     @archived = true if can_be_archived?
