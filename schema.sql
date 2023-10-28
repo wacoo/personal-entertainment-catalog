3253e37288a2af21bccde8f02d7ee08c3cc9bad5
Bett Enock <bettenock968@gmail.com>
2023-10-28 13:52:47 -0700

    Update catalog schema.

--- schema.sql
+++ schema.sql
@@ -1,8 +1,23 @@
+CREATE TABLE item (
+    id SERIAL PRIMARY KEY,
+    genre_id INT,
+    source_id INT,
+    author_id INT,
+    label_id INT,
+    publish_date DATE NOT NULL,
+    archived BOOLEAN DEFAULT false,
+
+    FOREIGN KEY (label_id) REFERENCES labels (id),  
+    FOREIGN KEY (genre_id) REFERENCES genres (id),
+    FOREIGN KEY (author_id) REFERENCES author (id),
+    FOREIGN KEY (source_id) REFERENCES sources (id)
+
+)
 CREATE TABLE movies (
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     published_date DATE NOT NULL,
-    silent BOOLEAN NOT NULL,
+    silent BOOLEAN DEFAULT false,,
     source_id INTEGER,
     FOREIGN KEY(source_id) REFERENCES sources(id)
 );
@@ -21,10 +36,12 @@ CREATE TABLE author (
 CREATE TABLE game (
     id INTEGER PRIMARY KEY,
     publish_date DATE,
-    multiplayer BOOLEAN,
+    multiplayer BOOLEAN DEFAULT false,,
     last_played_at DATE
     label_id INTEGER,
     genre_id INTEGER
+    FOREIGN KEY (genre_id) REFERENCES genres (id),
+    FOREIGN KEY (label_id) REFERENCES labels (id)
 );
 
 CREATE TABLE books (
@@ -34,16 +51,29 @@ CREATE TABLE books (
     cover_state VARCHAR(255) NOT NULL,
     genre_id INTEGER,
     label_id INTEGER,
-    source_id INTEGER,
     author_id INTEGER,
-    CONSTRAINT fk_genre FOREIGN KEY (genre_id) REFERENCES genres (id),
-    CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES authors (id),
-    CONSTRAINT fk_label FOREIGN KEY (label_id) REFERENCES labels (id)
-    CONSTRAINT fk_source FOREIGN KEY (source_id) REFERENCES sources (id)
+    FOREIGN KEY (genre_id) REFERENCES genres (id),
+    FOREIGN KEY (author_id) REFERENCES authors (id),
+    FOREIGN KEY (label_id) REFERENCES labels (id)
+);
+
+CREATE TABLE music_album (
+    id SERIAL PRIMARY KEY,
+    published_date DATE NOT NULL,
+    on_spotify BOOLEAN DEFAULT false,
+    genre_id INTEGER,
+    label_id INTEGER,
+    FOREIGN KEY (genre_id) REFERENCES genres (id),
+    FOREIGN KEY (label_id) REFERENCES labels (id)
 
 );
 
 CREATE TABLE labels (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE genres (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL
 );
